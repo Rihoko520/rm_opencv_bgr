58867eb4b64fd2f788271e31ef8f3f2b242f1dff
“Rihoko” <a1219814581@163.com>
2024-10-19 20:31:18 +0800

    fix

--- src/cam.py
+++ src/cam.py
@@ -4,7 +4,7 @@ import adjust  # 导入调试代码
 
 global mode, image_path, url, val, video
 mode = 0  # 模式设置 0: 视频流调试 1: 仅运行检测 2: 仅运行检测-无图 3: 静态图调试
-video = False # 是否识别视频
+video = True # 是否识别视频
 url = './photo/test.mp4'
 image_path = './photo/red_2.jpg'  # 图像路径
 val = 35  # 静态值
@@ -21,62 +21,6 @@ def get_first_available_camera():
 camera_index = get_first_available_camera()  # 获取可用摄像头
 if camera_index is None:
     print("错误: 没有找到可用的摄像头。")
-
-if video:
-    camera_index = url
-
-if mode == 0:  # 处理视频流
-    video_stream = cv2.VideoCapture(camera_index)
-    if not video_stream.isOpened():
-        print("错误: 无法打开视频流。")
-    adjust.setup_windows()  # 创建滑动条窗口
-    while True:
-        ret, frame = video_stream.read()
-        if not ret:
-            print("错误: 无法读取帧")
-            break
-        current_val = cv2.getTrackbarPos('Threshold', 'Threshold')
-        armors_dict = track_armor(frame, current_val, 2)
-        if armors_dict:
-            print(armors_dict)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    video_stream.release()
-    cv2.destroyAllWindows()
-
-elif mode in {1, 2}:  # 仅运行检测或仅运行检测-无图
-    video_stream = cv2.VideoCapture(camera_index)  # 使用可用摄像头
-    if not video_stream.isOpened():
-        print("错误: 无法打开摄像头。")
-    while True:
-        ret, frame = video_stream.read()
-        if not ret:
-            print("错误: 无法读取帧")
-            break           
-        armors_dict = track_armor(frame, val, 3 if mode == 2 else 2)   
-        if armors_dict:
-            print(armors_dict)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    video_stream.release()
-    cv2.destroyAllWindows()
-
-elif mode == 3:  # 实时处理静态图像
-    current_frame = cv2.imread(image_path)
-    if current_frame is None:
-        print("错误: 无法读取图像。请检查路径:", image_path)
-    adjust.setup_windows()  # 创建滑动条窗口
-    while True:
-        current_val = cv2.getTrackbarPos('Threshold', 'Threshold')
-        armors_dict = track_armor(current_frame, current_val, 2)
-        if armors_dict:
-            print(armors_dict)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
-
-else:
-    print("无效的模式，程序结束。")
 
 if video:
     camera_index = url
